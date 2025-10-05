5458fb5d68e8a886962f177dca7cba6354958ab3
Shoyeb45 <shoyebff45@gmail.com>
2025-10-05 21:33:40 +0530

    added _error page as it was giving build error

---
+++ frontend/src/app/_error.tsx
@@ -0,0 +1,17 @@
+// app/_error.tsx (for App Router)
+'use client';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
