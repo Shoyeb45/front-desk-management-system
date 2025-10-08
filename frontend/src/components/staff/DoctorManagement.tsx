a8d9f90002f2b4e8c98b9107dd7d1368462cd38a
Shoyeb45 <shoyebff45@gmail.com>
2025-10-08 16:15:13 +0530

    some build warning fixes

--- frontend/src/components/staff/DoctorManagement.tsx
+++ frontend/src/components/staff/DoctorManagement.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import { EmployeeList } from "../admin/EmployeeList";
 import { ManagementCard } from "../admin/ManagementCard";
 
 export function DoctorManagement() {
-    const [searchTerm, useSearchTerm] = useState("");
-    const [updateList, setUpdateList] = useState(0);
-
     return (
         <div className="py-4 ">
             <ManagementCard role="doctor" />
